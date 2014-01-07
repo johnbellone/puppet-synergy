de5a44676b9aa7e52e34a004a121df24e9742233
John Bellone <john.bellone.jr@gmail.com>
2014-01-07 11:16:06 -0500

    Update version of Synergy to latest.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 class synergy {
   package { 'Synergy':
     provider => 'appdmg',
-    source   => 'http://synergy.googlecode.com/files/synergy-1.4.11-MacOSX108-x86_64.dmg'
+    source   => 'http://synergy.googlecode.com/files/synergy-1.4.15-MacOSX108-x86_64.dmg'
   }
 }
